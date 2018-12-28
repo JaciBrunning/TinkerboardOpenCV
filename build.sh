43f9c845238d561500745266b2693bb24c25c880
Jaci Brunning <jaci.brunning@gmail.com>
2018-12-28 21:04:31 +0800

    Fix azure package release

--- build.sh
+++ build.sh
@@ -5,9 +5,6 @@
 # Install Tinkerboard Toolchain
 curl -SL https://github.com/JacisNonsense/TinkerboardGCC/releases/download/20181227.8/Tinkerboard-Linux-Toolchain-6.3.0.tar.gz | sh -c 'mkdir -p /usr/local && cd /usr/local && tar xzf - --strip-components=1'
 
-# export PATH=/tmp/tinkerboard-toolchain:$PATH
-# echo $PATH
-
 # Clone, patch and build thirdparty-opencv from wpilib
 git clone --recurse-submodules --depth=1 --shallow-submodules https://github.com/wpilibsuite/thirdparty-opencv.git
 pushd thirdparty-opencv
